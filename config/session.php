16a1730d48d610dc37d3fdc150a8c66c3e2c7659
RaRAshed <rnrashedrn@gmail.com>
2025-03-15 10:42:33 -0700

    updated to laravel 10

--- config/session.php
+++ config/session.php
@@ -1,5 +1,7 @@
 <?php
 
+use Illuminate\Support\Str;
+
 return [
 
     /*
@@ -122,9 +124,9 @@ return [
     |
     */
 
-    'cookie' => env(
+   'cookie' => env(
         'SESSION_COOKIE',
-        str_slug(env('APP_NAME', 'laravel'), '_').'_session'
+        Str::slug(env('APP_NAME', 'laravel'), '_').'_session'
     ),
 
     /*
